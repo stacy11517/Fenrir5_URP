803b13cee7876451c86acab7e70808efff4a47f6
DESKTOP-B941NOS\stacy <stacy11517@gmail.com>
2024-10-05 16:41:54 +0800

    ft.攻擊新增範圍偵測

--- My project/Assets/Scripts/PlayerAttack.cs
+++ My project/Assets/Scripts/PlayerAttack.cs
@@ -3,7 +3,13 @@
 public class PlayerAttack : MonoBehaviour
 {
     public int attackDamage = 10;  // 攻击力
+    public Collider attackCollider; // 攻擊部位的 Collider (例如爪子或嘴巴)
 
+    void Start()
+    {
+        // 初始時關閉攻擊的 Collider
+        attackCollider.enabled = false;
+    }
     void OnTriggerEnter(Collider other)
     {
         // 检查碰撞对象是否是敌人
